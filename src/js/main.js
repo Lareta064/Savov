3e1bae5aff26d8aa2d1c999a64b830e9799330a3
Lareta064 <lareta064@gmail.com>
2024-10-22 13:33:13 +0300

    start

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+	/*======переключение активного класса ====*/ 
+	function toggleActiveClass(parentClass, childClass) {
+		const parents = document.querySelectorAll('.' + parentClass);
+		parents.forEach(parent => {
+			parent.addEventListener('click', function (e) {
+
+				if (e.target.classList.contains(childClass)) {
+					// Удаляем класс 'active' у всех дочерних элементов внутри родителя
+					parent.querySelectorAll('.' + childClass).forEach(child => {
+						child.classList.remove('active');
+					});
+					// Добавляем класс 'active' элементу, по которому был клик
+					e.target.classList.add('active');
+				}
+			});
+		});
+	}
+	toggleActiveClass('tags-wrapper', 'tag')
+	
+
+});
