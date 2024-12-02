e84adda980b243094693c37813544852f5de7f6f
Lareta064 <lareta1@yandex.ru>
2024-12-02 22:03:06 +0300

    fix

--- src/js/page-swiper-slider.js
+++ src/js/page-swiper-slider.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-	
+    const resultSlider = new Swiper('.result-slider', {
+        pagination: {
+            el: ".swiper-pagination",
+			clickable: true,
+        },
+    });
+
+    resultSlider.on('slideChange', function () {
+        // Получаем индекс активного слайда
+        const activeIndex = resultSlider.activeIndex;
+
+        // Находим все элементы .company-name
+        const companyNames = document.querySelectorAll('.company-name');
+
+        // Удаляем класс 'active' у всех .company-name
+        companyNames.forEach(name => name.classList.remove('active'));
+
+        // Добавляем класс 'active' элементу с текущим индексом
+        if (companyNames[activeIndex]) {
+			companyNames[activeIndex].classList.add('active');
+        }
+    });
 });
